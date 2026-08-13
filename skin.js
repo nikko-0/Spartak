08d8e41a526b662fccc060c97a4ece340df7d68e
Nikita <nikitakoh2001@gmail.com>
2026-08-13 19:07:43 +0400

    tiles 256px

--- skin.js
+++ skin.js
@@ -1,7 +1,7 @@
 // Garden Gnome Software - Skin
 // Pano2VR 8.0.4/22574
 // Filename: material_dusk2.ggsk
-// Generated 2026-08-13T14:19:38Z
+// Generated 2026-08-13T14:59:05Z
 
 function pano2vrSkin(player,base) {
 	player.addVariable('vis_sounds_splashscreen', 2, false, { ignoreInState: 1  });
